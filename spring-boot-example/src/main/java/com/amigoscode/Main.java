3d39918937daadc35a2ffc2645ec343b62ec5c8f
Zykininho <zykininho@gmail.com>
2024-03-10 20:54:49 +0600

    feature: added create request

--- spring-boot-example/src/main/java/com/amigoscode/Main.java
+++ spring-boot-example/src/main/java/com/amigoscode/Main.java
@@ -2,9 +2,7 @@ package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,22 @@ public class Main {
     @GetMapping
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
     }
 
 }
